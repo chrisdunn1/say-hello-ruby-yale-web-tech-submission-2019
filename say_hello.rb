3c8071372bef384dce3a00bc1a663b52f6e3a829
Christopher Dunn <christopherdunnlaw@gmail.com>
2019-04-28 15:34:30 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,8 @@
 # Build your say_hello method here
 # I call on the method, say_hello, and give it the string "Gabriela"
-def say_hello(name = "Hello Ruby Programmer!")
+require_relative './spec_helper'
+ describe "say_hello" do 	describe "say_hello" do 
+def say_hello(name="Hello Ruby Programmer!")
   print "Hello #{name}!"
 end
 
