d1bbc6acb88fc092bc010ff9ccca1dddfca49aef
Christopher Dunn <christopherdunnlaw@gmail.com>
2019-04-28 15:42:56 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -5,9 +5,10 @@ def say_hello(name)
 puts "Hello Kent Beck"
 end
 
- def say_hello(name="Ruby Programmer!")
+ def say_hello(name="Ruby Programmer")
   puts "Hello #{name}!"
 end
 
 say_hello()
 say_hello("Kent Beck")
+say_hello("Gabriela")
